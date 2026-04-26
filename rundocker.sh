824d631d2b66fd1ddd839d4d65f8c8427c6cbcc1
Martin Sang Do <dothanhsang1984@yahoo.com>
2026-04-26 22:08:49 +0700

    - add test case

--- rundocker.sh
+++ rundocker.sh
@@ -1,1 +1,7 @@
-docker run -p 8080:8080 -v jenkins_data:/var/jenkins_home jenkins/jenkins:lts
+# docker run -p 8080:8080 -v jenkins_data:/var/jenkins_home jenkins/jenkins:lts
+docker run -d \
+  -p 8080:8080 \
+  -v jenkins_data:/var/jenkins_home \
+  -v /var/run/docker.sock:/var/run/docker.sock \
+  --name jenkins-server \
+  jenkins/jenkins:lts
