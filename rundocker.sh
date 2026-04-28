df4921bd83b1cdbd68a2a6a506a06f53b9adc470
Martin Sang Do <dothanhsang1984@yahoo.com>
2026-04-28 14:43:47 +0700

    - update Jenkins settings when install with brew

--- rundocker.sh
+++ rundocker.sh
@@ -1,4 +1,7 @@
 # docker run -p 8080:8080 -v jenkins_data:/var/jenkins_home jenkins/jenkins:lts
+docker stop jenkins-server
+docker rm -f jenkins-server
+sudo chmod 666 /var/run/docker.sock
 docker run -d \
   -p 8080:8080 \
   -v jenkins_data:/var/jenkins_home \
